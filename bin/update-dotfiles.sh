b54e05b98ff2c5ca7e1906925279056036fb99d3
nuclearglow <svenvowe@gmail.com>
2018-09-06 09:41:58 +0200

    updated script, updated Brewfile, aliases code cleanup, added exports

--- bin/update-dotfiles.sh
+++ bin/update-dotfiles.sh
@@ -6,14 +6,12 @@ cp ~/.Brewfile ~/workspace/dotfiles
 # ~/bin folder
 cp ~/bin/*.sh ~/workspace/dotfiles/bin
 cp -R ~/.bash_it ~/workspace/dotfiles/
-cp -R ~/bin/git-aware-prompt ~/workspace/dotfiles/bin
+cp -R ~/bin/git-aware-prompt ~/workspace/dotfiles/bin/
 zip -r ~/workspace/dotfiles/bin/OpeniTermTab.app.zip ~/bin/OpeniTermTab.app
 cp -R ~/iterm-settings ~/workspace/dotfiles
 # vscode
 cp ~/Library/Application\ Support/Code/User/*.json ~/workspace/dotfiles/Library/Application\ Support/Code/User/
 cp -R ~/Library/Application\ Support/Code/User/snippets ~/workspace/dotfiles/Library/Application\ Support/Code/User/
-code --list-extensions | xargs -L 1 echo code --install-extension > .vscode-extensions
-cp ~/.vscode-extensions ~/workspace/dotfiles
 # dotfiles
 cp ~/.macos ~/workspace/dotfiles
 cp ~/.bash_profile ~/workspace/dotfiles
@@ -22,6 +20,6 @@ cp ~/.gitk ~/workspace/dotfiles
 cp ~/.nvmrc ~/workspace/dotfiles
 cp ~/.vimrc ~/workspace/dotfiles
 cp ~/.wgetrc ~/workspace/dotfiles
+cp ~/.exports ~/workspace/dotfiles
 cp ~/.aliases ~/workspace/dotfiles
 cp ~/.functions ~/workspace/dotfiles
-
